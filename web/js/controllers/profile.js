6cfd23db83499a7d70dc6b4f4e23451f72410085
Michael Mayer <michael@liquidbytes.net>
2014-07-31 13:36:10 +0200

    Improved profile page

--- web/js/controllers/profile.js
+++ web/js/controllers/profile.js
@@ -1,8 +1,14 @@
 define(['controllers/module'], function (controllers) {
     'use strict';
-    return controllers.controller('ProfileController', ['$scope', '$routeParams', 'Api', function ($scope, $routeParams, api) {
-        var userResource = api.getUserResource();
+    return controllers.controller('ProfileController', ['$scope', '$routeParams', 'Api', 'Alert', function ($scope, $routeParams, Api, Alert) {
+        var userResource = Api.getUserResource();
 
         $scope.user = userResource.get({user_id: $routeParams.userId});
+
+        $scope.update = function () {
+            this.user.$update(function () {
+                Alert.success('Profile successfully saved');
+            });
+        }
     }]);
 });
--- web/js/controllers/user.js
+++ web/js/controllers/user.js
@@ -1,15 +1,19 @@
 define(['controllers/module'], function (controllers) {
     'use strict';
-    return controllers.controller('UserController', ['$scope', 'Api', '$modalInstance', 'user', function ($scope, api, $modalInstance, user) {
+    return controllers.controller('UserController', ['$scope', 'Alert', '$modalInstance', 'user', function ($scope, Alert, $modalInstance, user) {
         $scope.user = user;
 
         $scope.update = function () {
-            this.user.$update();
+            this.user.$update(function () {
+                Alert.success('User successfully updated');
+            });
             $modalInstance.close();
         }
 
         $scope.create = function () {
-            this.user.$save();
+            this.user.$save(function () {
+                Alert.success('User successfully created');
+            });
             $modalInstance.close(this.user);
         }
 
