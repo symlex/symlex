40406f1e8f8cb095f39dd391a5e8beb770c2cd1f
Michael Mayer <michael@liquidbytes.net>
2014-07-31 13:05:24 +0200

    Removed app_dev.php (because it's useless for a single page apps)

--- web/app.php
+++ web/app.php
@@ -2,8 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$debugMode = !isset($_SERVER['HTTP_CLIENT_IP'])
+    && !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.56.1'));
+
 use App\HttpApp;
 
-$app = new HttpApp (__DIR__ . '/../app', false);
+$app = new HttpApp (__DIR__ . '/../app', $debugMode);
 
 $app->run();
