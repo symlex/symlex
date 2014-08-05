6381be0a4e66c0f85e3907270a7794472adee610
Michael Mayer <michael@liquidbytes.net>
2014-08-05 11:02:01 +0200

    Improved navbar login

--- web/js/controllers/navigation.js
+++ web/js/controllers/navigation.js
@@ -1,6 +1,11 @@
 define(['controllers/module'], function (controllers) {
     'use strict';
     return controllers.controller('NavigationController', ['$scope', '$location', function ($scope, $location) {
+        $scope.login = {
+            email: '',
+            password: ''
+        };
+
         $scope.isActive = function (viewLocation) {
             var path = $location.path();
             return path.indexOf(viewLocation) === 0;
