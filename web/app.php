8acee3a7625dfad2bdf9a5caf759f1e5f40ed400
Michael Mayer <michael@liquidbytes.net>
2014-08-20 12:55:38 +0200

    Disabled ApcClassLoader in debug mode

--- web/app.php
+++ web/app.php
@@ -2,17 +2,17 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 
+$debugMode = !isset($_SERVER['HTTP_CLIENT_IP'])
+    && !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.56.1'));
+
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 
-if (extension_loaded('apc') && ini_get('apc.enabled') == '1') {
+if (!$debugMode && extension_loaded('apc') && ini_get('apc.enabled') == '1') {
     $apcLoader = new ApcClassLoader(__DIR__, $loader);
     $loader->unregister();
     $apcLoader->register(true);
 }
-
-$debugMode = !isset($_SERVER['HTTP_CLIENT_IP'])
-    && !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.56.1'));
 
 use App\HttpApp;
 
