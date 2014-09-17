060a3d53264cb4901d90ce3744102ef2cdb9de87
Michael Mayer <michael@liquidbytes.net>
2014-09-17 14:21:21 +0200

    Improved local IP detection (for debug mode)

--- web/app.php
+++ web/app.php
@@ -4,7 +4,9 @@ use Symfony\Component\ClassLoader\ApcClassLoader;
 
 $debugMode = !isset($_SERVER['HTTP_CLIENT_IP'])
     && !isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    && in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1', '192.168.56.1'));
+    && (in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) ||
+        (ip2long(@$_SERVER['REMOTE_ADDR']) & ((256*256-1) << 16)) == ((192 << 24) | (168 << 16))
+    );
 
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
 
