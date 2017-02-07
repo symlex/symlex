9da8c2eea1233c6c17c29c515a6617421769dd94
Michael Mayer <michael@liquidbytes.net>
2017-02-07 16:23:14 +0200

    Using HTTP_HOST instead of SERVER_NAME

--- web/app.php
+++ web/app.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symlex\Bootstrap\WebApp;
 
-$debugMode = strpos($_SERVER['SERVER_NAME'], '-debug') !== false;
+$debugMode = strpos($_SERVER['HTTP_HOST'], '-debug') !== false;
 
 $app = new WebApp (__DIR__ . '/../app', $debugMode);
 
