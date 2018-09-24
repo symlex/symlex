3159b3521abd70a572eadb9fc492356677aa458f
Theresa Gresch <theresagresch@gmail.com>
2018-09-24 11:23:08 +0200

    Used vuetify for navigation

--- frontend/src/component/app-navigation.vue
+++ frontend/src/component/app-navigation.vue
@@ -1,43 +1,117 @@
 <template>
     <div class="app-navigation">
-        <md-toolbar md-theme="navigation">
-            <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
-                <md-icon>menu</md-icon>
-            </md-button>
 
-            <h2 class="md-title">{{ $config.appName }}</h2>
+    <v-navigation-drawer
+                v-model="drawer"
+                :mini-variant="mini"
+                fixed
+                dark
+                app
+                width="200"
+                permanent
+    >
+        <v-toolbar flat class="pb-2">
+            <v-list class="pb-2">
+                <v-list-tile avatar>
+                    <v-list-tile-avatar>
 
-            <span style="flex: 1"></span>
+                    </v-list-tile-avatar>
 
-            <md-button v-if="$session.isAnonymous()" @click.native="login()" class="md-raised md-accent">Login</md-button>
-            <md-button v-if="$session.isAnonymous()" @click.native="register()" class="md-raised md-accent">Register</md-button>
-            <md-button v-if="$session.isUser()" @click.native="logout()" class="md-raised md-accent">Logout {{ $session.getFirstName() }}</md-button>
-        </md-toolbar>
+                    <v-list-tile-content>
+                        <v-list-tile-title class="title">{{ $config.appName }}</v-list-tile-title>
+                    </v-list-tile-content>
 
-        <md-sidenav class="md-left md-fixed" ref="leftSidenav" :md-swipeable="true" @open="open('Left')"
-                    @close="close('Left')">
-            <md-list>
-                <md-list-item @click.native="$refs.leftSidenav.toggle()">
-                    <router-link to="/welcome">Welcome</router-link>
-                </md-list-item>
+                    <v-list-tile-action>
+                        <v-btn
+                            icon
+                            @click.stop="mini = !mini"
+                        >
+                            <v-icon>chevron_left</v-icon>
+                        </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+        </v-toolbar>
 
-                <md-list-item v-if="$session.isAnonymous()" @click.native="$refs.leftSidenav.toggle()">
-                    <router-link to="/register">Register</router-link>
-                </md-list-item>
+        <v-list>
 
-                <md-list-item v-if="$session.isAdmin()" @click.native="$refs.leftSidenav.toggle()">
-                    <router-link to="/users">Users</router-link>
-                </md-list-item>
+          <v-list-tile v-if="mini" @click.stop="mini = !mini">
+              <v-list-tile-action>
+                  <v-icon>chevron_right</v-icon>
+              </v-list-tile-action>
+          </v-list-tile>
 
-                <md-list-item v-if="$session.isUser()" @click.native="$refs.leftSidenav.toggle()">
-                    <router-link to="/profile/details">Profile</router-link>
-                </md-list-item>
+          <v-list-tile to="/welcome" @click="">
+            <v-list-tile-action>
+                <v-icon>home</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+                <v-list-tile-title>Welcome</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
 
-                <md-list-item v-if="$session.isUser()" @click.native="$refs.leftSidenav.toggle()">
-                    <router-link to="/profile/password">Change Password</router-link>
-                </md-list-item>
-            </md-list>
-        </md-sidenav>
+          <v-list-tile  v-if="$session.isAnonymous()"  to="loginDialog" @click.native="login()">
+            <v-list-tile-action>
+                <v-icon>input</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>Login</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile  v-if="$session.isAnonymous()" to="/register" @click="">
+            <v-list-tile-action>
+                <v-icon>create</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>Register</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile  v-if="$session.isAdmin()" to="/users" @click="">
+            <v-list-tile-action>
+                <v-icon>group</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>Users</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile  v-if="$session.isAdmin()" to="/profile/details" @click="">
+            <v-list-tile-action>
+                <v-icon>face</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>Profile</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile  v-if="$session.isAdmin()" to="/profile/password" @click="">
+            <v-list-tile-action>
+                <v-icon>settings</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>Change Password</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+
+          <v-list-tile  v-if="$session.isUser()" to="/" @click.native="logout()">
+            <v-list-tile-action>
+                <v-icon>power_settings_new</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>Logout</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+      </v-navigation-drawer>
 
         <app-login-dialog ref="loginDialog"></app-login-dialog>
     </div>
@@ -46,7 +120,17 @@
 <script>
     export default {
         data() {
-            return {};
+            return {
+                drawer: true,
+                items: [
+                    { title: 'Welcome', route: 'welcome', icon: 'dashboard'},
+                    { title: 'Register', route: 'register'},
+                    { title: 'Users', route: 'users'},
+                    { title: 'Profile', route: 'profile/details'},
+                    { title: 'Change Password', route: 'profile/password'},
+                 ],
+                mini: true,
+             };
         },
         methods: {
             toggleLeftSidenav() {
